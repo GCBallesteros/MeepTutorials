57d1ea8655be6434bb4c5a997ca12c41aa555e06
mariswow <m.cizauskas38@gmail.com>
2020-11-26 22:51:00 +0200

    Added epsilon overlay

--- frequency_monitors/show_freq_monitor.py
+++ frequency_monitors/show_freq_monitor.py
@@ -7,7 +7,14 @@ import matplotlib
 matplotlib.rcParams["backend"] = "TkAgg"
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+from matplotlib.colors import ListedColormap
 
+# Parameters for epsilon
+default_eps_parameters = {
+		'interpolation':'spline36',
+		'alpha':1.0,
+		'aspect':'equal'
+	}
 
 def find_nearest(array, value):
     idx = (np.abs(array-value)).argmin()
@@ -26,21 +33,36 @@ def sigmoid_apodization(t, t0, tau):
     return 1 / (1 + np.exp(-(t-t0)/tau))
 
 def load_monitor(fname):
-    f = h5py.File(fname, 'r')
+	f = h5py.File(fname, 'r')
 
-    field = np.array(f[list(f.keys())[0]])
+	field = np.array(f[list(f.keys())[0]])
 
-    metadata = {'max_freq': f.attrs.get('maxfreq'),
-                'min_freq': f.attrs.get('minfreq'),
-                'dt': f.attrs.get('dt'),
-                'xmin': f.attrs.get('xmin'),
-                'xmax': f.attrs.get('xmax'),
-                'ymin': f.attrs.get('ymin'),
-                'ymax': f.attrs.get('ymax')}
+	metadata = {'max_freq': f.attrs.get('maxfreq'),
+				'min_freq': f.attrs.get('minfreq'),
+				'dt': f.attrs.get('dt'),
+				'xmin': f.attrs.get('xmin'),
+				'xmax': f.attrs.get('xmax'),
+				'ymin': f.attrs.get('ymin'),
+				'ymax': f.attrs.get('ymax'),
+				'res':f.attrs.get('res')}
 
-    f.close()
+	f.close()
 
-    return field, metadata
+	return field, metadata
+
+def load_struc(ename):
+	# Load the epsilon
+	f = h5py.File(ename, 'r')
+	eps = np.array(f[list(f.keys())[0]])
+	f.close()
+	eshape = np.array(eps.shape)
+	
+	# Shape the epsilon variable depending on dimensions
+	if eps.ndim == 3:
+		eps = eps[:,::-1,int(eshape[2]/2)].T
+	else: eps = eps[:,::-1].T
+
+	return eps
 
 def transform_field(field, metadata, npoints, apodization_func=None):
     max_freq = metadata['max_freq']
@@ -72,44 +94,72 @@ def transform_field(field, metadata, npoints, apodization_func=None):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Plot frequency domain field profile monitor.')
-    parser.add_argument('--fname',
-                        help='Input h5 filename')
-    parser.add_argument('--npoints', default=100, type=int,
-                        help='Number of frequency points to compute. Spacing is uniform.')
-    parser.add_argument('--t0', default=0, type=float,
-                        help='Center of the apodization function.')
-    parser.add_argument('--tau', default=1e-10, type=float,
-                        help='Ramp up time of the apodization function.')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='Plot frequency domain field profile monitor.')
+	parser.add_argument('--fname',
+						help='Input h5 filename')
+	parser.add_argument('--ename',
+						help='Input h5 epsilon filename', default='')
+	parser.add_argument('--npoints', default=100, type=int,
+						help='Number of frequency points to compute. Spacing is uniform.')
+	parser.add_argument('--t0', default=0, type=float,
+						help='Center of the apodization function.')
+	parser.add_argument('--tau', default=1e-10, type=float,
+						help='Ramp up time of the apodization function.')
+	args = parser.parse_args()
 
-    (field, metadata) = load_monitor(args.fname)
+	(field, metadata) = load_monitor(args.fname)
 
-    apodize = lambda t: sigmoid_apodization(t, args.t0, args.tau)
-    (freqs, fft_field) = transform_field(field, metadata, args.npoints, apodize)
+	apodize = lambda t: sigmoid_apodization(t, args.t0, args.tau)
+	(freqs, fft_field) = transform_field(field, metadata, args.npoints, apodize)
+	
+	fig, ax = plt.subplots()
+	plt.subplots_adjust(left=0.1, bottom=0.25)
+	
+	my_cmap = plt.cm.viridis
 
-    # Plot Results
-    central_freq = (metadata['max_freq']+metadata['min_freq'])*0.5
+	# Check if epsilon argument supplied
+	if args.ename != '':
+		eps = load_struc(args.ename)
+		
+		# Choose colormap
+		cmap = plt.cm.Wistia
 
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(left=0.1, bottom=0.25)
-    field_plot = plt.imshow(fft_field[:,::-1, find_nearest(freqs, central_freq)].T,
-                            extent=[metadata['xmin'],
-                                    metadata['xmax'],
-                                    metadata['ymin'],
-                                    metadata['ymax']])
-    plt.colorbar()
+		# Get the colormap colors
+		my_cmap = cmap(np.arange(cmap.N))
 
-    axcolor = 'lightgoldenrodyellow'
-    axfreq = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor=axcolor)
-    sfreq = Slider(axfreq, 'Freq', np.min(freqs), np.max(freqs),
-                   valinit=central_freq, valfmt='%0.3f')
+		# Set alpha
+		my_cmap[:,-1] = np.linspace(0, 1, cmap.N)
 
-    def update(val):
-        freq = sfreq.val
-        field_plot.set_data(fft_field[:, ::-1, find_nearest(freqs, freq)].T)
-        field_plot.autoscale()
-        fig.canvas.draw_idle()
-    sfreq.on_changed(update)
+		# Create new colormap
+		my_cmap = ListedColormap(my_cmap)
+		
+		# Step colormap for epsilon structure
+		cmap = ListedColormap(["navy", "black"])
+		eps_plot = plt.imshow(eps, **default_eps_parameters, cmap=cmap,
+							extent=[metadata['xmin'],
+									metadata['xmax'],
+									metadata['ymin'],
+									metadata['ymax']])
+									
+	# Plot Results
+	central_freq = (metadata['max_freq']+metadata['min_freq'])*0.5
+	field_plot = plt.imshow(fft_field[:,::-1, find_nearest(freqs, central_freq)].T, cmap=my_cmap,
+							extent=[metadata['xmin'],
+									metadata['xmax'],
+									metadata['ymin'],
+									metadata['ymax']])
+	plt.colorbar()
 
-    plt.show()
+	axcolor = 'lightgoldenrodyellow'
+	axfreq = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor=axcolor)
+	sfreq = Slider(axfreq, 'Freq', np.min(freqs), np.max(freqs),
+				   valinit=central_freq, valfmt='%0.3f')
+
+	def update(val):
+		freq = sfreq.val
+		field_plot.set_data(fft_field[:, ::-1, find_nearest(freqs, freq)].T)
+		field_plot.autoscale()
+		fig.canvas.draw_idle()
+	sfreq.on_changed(update)
+
+	plt.show()
